0e4be23f3d3e565b3819c1ff947ca523490599b3
apple <developer@apple.com>
2026-07-15 10:27:48 +0200

    npm package: bump to v1.2.9

--- npm-package/postinstall.js
+++ npm-package/postinstall.js
@@ -4,7 +4,7 @@ const { execSync } = require('child_process');
 const os = require('os');
 
 const platform = os.platform();
-const version = '1.2.2';
+const version = '1.2.9';
 
 console.log('📦 Installing GlanceWatch v' + version + '...');
 console.log('Platform:', platform);
